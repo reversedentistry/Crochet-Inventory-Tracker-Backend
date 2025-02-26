bc406cb6dd86f050f98fee49ba084080a53ba62b
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2025-02-25 19:41:14 -0800

    Adding service methods

--- src/main/java/com/project/crochetpersonal/controller/HookController.java
+++ src/main/java/com/project/crochetpersonal/controller/HookController.java
@@ -1,7 +1,11 @@
 package com.project.crochetpersonal.controller;
 
+import com.project.crochetpersonal.service.HookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HookController {
+    @Autowired
+    HookService hookService;
 }
--- src/main/java/com/project/crochetpersonal/service/HookService.java
+++ src/main/java/com/project/crochetpersonal/service/HookService.java
@@ -1,7 +1,34 @@
 package com.project.crochetpersonal.service;
 
+import com.project.crochetpersonal.model.Hook;
+import com.project.crochetpersonal.repository.HookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class HookService {
+    @Autowired
+    HookRepository hookRepo;
+
+    public long totalHookCount() {
+        return hookRepo.count();
+    }
+
+    public List<Hook> findAllHooksBySize(double size) {
+        return hookRepo.findAllBySize(size);
+    }
+
+    public List<Hook> findAllHooksByMaterial(String material) {
+        return hookRepo.findAllByMaterial(material);
+    }
+
+    public List<Hook> findAllHooksByStyle(String style) {
+        return hookRepo.findAllByStyle(style);
+    }
+
+    public List<Hook> findAllTunisian() {
+        return hookRepo.findAllByIsTunisianTrue();
+    }
 }
